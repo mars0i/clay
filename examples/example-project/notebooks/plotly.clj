5661dd07b0642a05415029569096681f76e7ba7c
Marshall Abrams <marshall@logical.net>
2025-03-06 13:02:59 -0600

    Added test of LaTeX in Tableplot Plotly, with deps.edn changes.
    deps.edn hard-codes paths on my system. Would have to be fixed for
    others.

---
+++ examples/example-project/notebooks/plotly.clj
@@ -0,0 +1,19 @@
+(ns plotly
+  (:require [scicloj.kindly.v4.kind :as kind]
+            [scicloj.clay.v2.api :as clay]
+            [scicloj.tableplot.v1.plotly :as plotly]
+            [tablecloth.api :as tc]))
+
+(comment
+  (clay/make! {:source-path "notebooks/mwe.clj"})
+)
+
+(-> (tc/concat
+      (tc/dataset {:x [0 1], :y [0 1], :fun "$y=x^2$"})
+      (tc/dataset {:x [0 1], :y [1 0], :fun "y=-x"}))
+    (plotly/layer-line {:=x :x, :=y, :y :=color :fun})
+    (plotly/plot)
+    (assoc-in [:data 0 :name] "y<sup>2</sup> = -x")
+    (assoc-in [:data 1 :name] "$y^2 = -x$")
+    )
+
